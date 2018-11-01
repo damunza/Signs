915b4d67629b9842ae291f362046dbd6e946a5d3
damunza <danidamunza@gmail.com>
2018-11-01 15:21:56 +0300

    detects green objects

--- gesture.py
+++ gesture.py
@@ -16,18 +16,33 @@ while(True):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     #lower and upper extents of the colors to detect
+    # blue
     lower_blue = np.array([110, 50, 50])
     upper_blue = np.array([130, 255, 255])
 
+    #green
+    lower_green = np.array([33,80,40])
+    upper_green = np.array([100,255,255])
+
     #getting only blue images
     blue = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    #getting green object
+    green = cv2.inRange(hsv, lower_green, upper_green)
 
     #operations on the blue object
     blueopen=cv2.morphologyEx(blue, cv2.MORPH_OPEN, Open)
     blueclose=cv2.morphologyEx(blueopen, cv2.MORPH_CLOSE, Close)
 
+    #opperations on the green object
+    greenopen=cv2.morphologyEx(green, cv2.MORPH_OPEN, Open)
+    greenclose = cv2.morphologyEx(greenopen, cv2.MORPH_CLOSE, Close)
+
     #number of blues available
     _, bluecnts, h = cv2.findContours(blueclose.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)# if you get too many values to unpack just add _, at the begining
+
+    #number of greens available
+    _, greencnts, h = cv2.findContours(greenclose.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     font=cv2.FONT_HERSHEY_SIMPLEX
     trial=cv2.LINE_AA
@@ -59,6 +74,9 @@ while(True):
 
         #output for the situation
         cv2.putText(frame, 'B', (0, 50), font, 2, (0, 255, 0), 3, trial)
+
+    elif (len(greencnts)==1):
+        cv2.putText(frame, 'green', (0, 50), font, 2, (0, 0, 255), 3, trial)
 
 
     # displaying the frame captured
